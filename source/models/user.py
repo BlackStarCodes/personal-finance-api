94400e8dd7d3646ed3a2005715fe758547e14b8d
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-07 15:53:18 +0530

    feat: made model folder to have designated models and added user table definitions in user.py

---
+++ source/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, func
+from ..database import Base
+from datetime import datetime
+
+
+
+class UserOrm(Base):
+    __tablename__ = 'registered_users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30), unique=True)
+    fullname: Mapped[str | None]
+    email: Mapped[str] = mapped_column(String(60), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
