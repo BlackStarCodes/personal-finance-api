05fd9c6a43550b5f8f5e4e089eb6b366c44a4449
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-07 14:03:41 +0530

    feat: setup root endpoint to test api and established database connection with sqlalchemy

---
+++ source/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
+from .config import URL
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_engine(URL)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
