cf87388e9126dc20f6753eadc7a6612c58b43e8d
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-08 16:39:43 +0530

    feat: implemented get current_user endpoint

--- source/routers/users.py
+++ source/routers/users.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, HTTPException
-from ..schemas import UserRegister, UserOut
+from fastapi import APIRouter, HTTPException, Depends
+from ..schemas import UserRegister, UserOut, UserCreate
 from ..models.user import UserOrm
 from ..dependencies import session_dependency
-from ..security import pwd_hashed
+from ..security import pwd_hashed, get_current_user
 from sqlalchemy import select
+from typing import Annotated    
+
 
 router = APIRouter()
 
@@ -24,3 +26,8 @@ async def create_user(user:UserRegister, session: session_dependency):
     session.commit()
     session.refresh(new_user)
     return new_user
+
+
+@router.get('/me', response_model=UserOut)
+async def read_current_user(user: Annotated[UserOrm, Depends(get_current_user)]):
+    return user
--- source/security.py
+++ source/security.py
@@ -1,14 +1,18 @@
 import jwt
+from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
 from .dependencies import session_dependency, oauth2_scheme
 from .models.user import UserOrm
 from sqlalchemy import select
 from datetime import datetime, timedelta, timezone
 from .config import SECRET_KEY, ALGO
-
+from typing import Annotated
+from fastapi import HTTPException, status, Depends
+from .schemas import TokenData
 
 
 hasher = PasswordHash.recommended()
+
 
 def pwd_hashed(pwd) -> str:
     return hasher.hash(pwd)
@@ -45,3 +49,22 @@ def create_access_token(data: dict, expire_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGO)
     return encoded_jwt
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: session_dependency):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate Credentials!", headers={"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+        sub = payload.get("sub")
+        if not sub:
+            raise credentials_exception
+        user_id = int(sub)
+        if not user_id:
+            raise credentials_exception
+        token_data = TokenData(id= user_id)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = session.get(UserOrm, token_data.id)
+    if not user:
+        raise credentials_exception
+    return user
