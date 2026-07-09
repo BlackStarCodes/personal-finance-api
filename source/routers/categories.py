fb3088dcf30c26d5aa6c53402692a11495348e4f
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-09 17:44:08 +0530

    feat: implemented read_categories end points

--- source/routers/categories.py
+++ source/routers/categories.py
@@ -33,3 +33,33 @@ async def create_category(
     session.commit()
     session.refresh(new_category)
     return new_category
+
+
+@router.get('/', response_model=list[CategoryOut])
+async def read_categories(
+    user: current_user,
+    session: session_dependency
+):
+    categories = session.scalars(select(CategoryOrm).where(
+        CategoryOrm.user_id == user.id
+    )).all()
+
+    return categories
+
+
+@router.get('/{category_id}', response_model=CategoryOut)
+async def read_category(
+    user: current_user,
+    category_id: int,
+    session: session_dependency,
+):
+    category = session.scalar(select(CategoryOrm).where(
+        CategoryOrm.id == category_id, 
+        CategoryOrm.user_id == user.id))
+    
+    if not category:
+        raise HTTPException(status_code=404, detail="Category Not Found!")
+    
+    return category
+
+
