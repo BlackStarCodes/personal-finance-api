1016027ec4cdb7918253a5567ea2e7e4b4defc63
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-10 12:23:57 +0530

    chore: added name column and default values to transaction table

--- source/models/transaction.py
+++ source/models/transaction.py
@@ -10,17 +10,18 @@ class TransactionOrm(Base):
     __tablename__ = "transactions"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
     to_wallet_id: Mapped[int | None] = mapped_column(ForeignKey('wallets.id'))
     from_wallet_id: Mapped[int | None] = mapped_column(ForeignKey('wallets.id'))
     category_id: Mapped[int | None] = mapped_column(ForeignKey('categories.id'))
     amount: Mapped[Decimal] = mapped_column(Numeric(12,2))
     transaction_date: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default= func.now(), onupdate=func.now())    
-    description: Mapped[str] = mapped_column(String(400))
+    description: Mapped[str | None] = mapped_column(String(400))
     merchant: Mapped[str | None] = mapped_column(String(150))
     transaction_medium: Mapped[TransactionMedium] = mapped_column(Enum(TransactionMedium))
-    status: Mapped[TransactionStatus] = mapped_column(Enum(TransactionStatus))
-    is_recurring: Mapped[bool] = mapped_column(Boolean)
+    status: Mapped[TransactionStatus] = mapped_column(Enum(TransactionStatus), server_default="completed")
+    is_recurring: Mapped[bool] = mapped_column(Boolean, server_default="false", nullable=False)
     receipt_url: Mapped[str | None] = mapped_column(String(500))
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), 
