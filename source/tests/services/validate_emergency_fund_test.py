9d866e60afeabfe492598fd44ede0dd1734892c2
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-18 14:04:40 +0530

    test(validate_emergency_fund): add missed validation test cases

--- source/tests/services/validate_emergency_fund_test.py
+++ source/tests/services/validate_emergency_fund_test.py
@@ -4,6 +4,24 @@ import source.services.transaction_service as ts
 from source.enums import WalletGroup
 
 
+def test_emergency_fund_requires_source_wallet(wallet_factory):
+    emergency_fund_wallet = wallet_factory(WalletGroup.EMERGENCY_FUND)
+    with pytest.raises(HTTPException) as exc:
+        ts.validate_emergency_fund(from_wallet=None, to_wallet=emergency_fund_wallet)
+
+    assert exc.value.status_code == 400
+    assert exc.value.detail == "Emergency Fund transaction needs a source wallet!"
+
+
+def test_emergency_fund_requires_destination_wallet(wallet_factory):
+    emergency_fund_wallet = wallet_factory(WalletGroup.EMERGENCY_FUND)
+    with pytest.raises(HTTPException) as exc:
+        ts.validate_emergency_fund(from_wallet=emergency_fund_wallet, to_wallet=None)
+
+    assert exc.value.status_code == 400
+    assert exc.value.detail == "Emergency Fund transaction needs a destination wallet!"
+
+
 VALID_PAIRS = [
         (WalletGroup.AVAILABLE, WalletGroup.EMERGENCY_FUND),
         (WalletGroup.UNASSIGNED, WalletGroup.EMERGENCY_FUND),
