b48edd9ec9079cae7a749add345fefc5b638bf54
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-18 13:40:14 +0530

    test(validate-savings): add validation test cases

---
+++ source/tests/services/validate_savings_test.py
@@ -0,0 +1,53 @@
+import pytest
+from fastapi import HTTPException
+import source.services.transaction_service as ts
+from source.enums import WalletGroup
+
+
+VALID_PAIRS = [
+        (WalletGroup.AVAILABLE, WalletGroup.SAVINGS),
+        (WalletGroup.UNASSIGNED, WalletGroup.SAVINGS),
+        (WalletGroup.SAVINGS, WalletGroup.AVAILABLE),
+        (WalletGroup.SAVINGS, WalletGroup.UNASSIGNED),
+        (WalletGroup.INVESTMENT, WalletGroup.SAVINGS),
+        (WalletGroup.EMERGENCY_FUND, WalletGroup.SAVINGS),]
+
+@pytest.mark.parametrize("from_wallet_group, to_wallet_group", VALID_PAIRS)
+
+def test_savings_accepts_valid_wallet_groups(from_wallet_group, to_wallet_group, wallet_factory):
+     
+     from_wallet = wallet_factory(from_wallet_group)
+     to_wallet = wallet_factory(to_wallet_group)
+
+     ts.validate_savings(from_wallet, to_wallet)
+
+
+
+INVALID_PAIRS = [
+        (WalletGroup.DEBT, WalletGroup.SAVINGS),
+        (WalletGroup.SAVINGS, WalletGroup.DEBT),
+        (WalletGroup.SAVINGS, WalletGroup.SAVINGS),
+        (WalletGroup.SAVINGS, WalletGroup.INVESTMENT),
+        (WalletGroup.AVAILABLE, WalletGroup.UNASSIGNED),
+        (WalletGroup.INVESTMENT, WalletGroup.EMERGENCY_FUND),
+        (WalletGroup.SAVINGS, WalletGroup.EMERGENCY_FUND),
+        (WalletGroup.UNASSIGNED, WalletGroup.UNASSIGNED),
+        (WalletGroup.AVAILABLE, WalletGroup.EMERGENCY_FUND),
+        (WalletGroup.UNASSIGNED, WalletGroup.DEBT),
+        (WalletGroup.INVESTMENT, WalletGroup.AVAILABLE),
+        (WalletGroup.DEBT, WalletGroup.INVESTMENT),
+        ]
+
+
+@pytest.mark.parametrize("from_wallet_group, to_wallet_group", INVALID_PAIRS)
+
+def test_savings_rejects_invalid_wallet_groups(from_wallet_group, to_wallet_group, wallet_factory):
+     
+    from_wallet = wallet_factory(from_wallet_group)
+    to_wallet = wallet_factory(to_wallet_group)
+
+    with pytest.raises(HTTPException) as exc:
+        ts.validate_savings(from_wallet, to_wallet)
+
+    assert exc.value.status_code == 400
+    assert exc.value.detail == "Invalid Savings transaction!"
